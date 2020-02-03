3000b0b046f7b8ab5aeb6d8a50050b4876bdfe74
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 21:57:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
-
-
 def prime?(num)
-  return false if num < 2 
-  return true if num  == 3 || num  == 2 
+   if num < 2 
+     return false
+   if num  == 3 || num  == 2 
+     return true
     if (2...num -1).any?{|n| num % n == 0}
       false
     else
